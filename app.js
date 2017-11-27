e0b40c4f1c7254b9fa5df6ed602f39661299d3c7
Mauricio Giordano <mauricio.c.giordano@gmail.com>
2017-11-27 01:17:04 -0200

    Added verification user script

--- app.js
+++ app.js
@@ -7,6 +7,28 @@ const koa        = new (require('koa'))
 const routes  = require('./route/index.js')
 	, User    = require('./model/user.js')
 
+// Verifies if main user exists
+async function verify() {
+	let user = await User.findOne({
+		where : {
+			email : 'mauricio.c.giordano@gmail.com'
+		},
+		raw : true
+	})
+
+	if (user == null) {
+		await User.create({
+			name : 'Mauricio Giordano',
+			email : 'mauricio.c.giordano@gmail.com',
+			password : '3b55f356c3a028eca254962faac9020d04340eb45d8498aeeda882bf8d11914d',
+			parkId : null,
+			admin : true
+		})
+	}
+}
+
+verify()
+
 // Builds all routes
 routes(router)
 
