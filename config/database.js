06c662d895c8645efe1b52f73da8f22406e76576
Mauricio Giordano <mauricio.c.giordano@gmail.com>
2017-11-27 01:25:58 -0200

    Added database credentials

--- config/database.js
+++ config/database.js
@@ -1,5 +1,10 @@
 
-module.exports = new (require('sequelize'))('waterpark', 'root', '', {
-	host    : 'localhost',
+let host = process.env.DATABASE_HOST || 'localhost'
+let user = process.env.DATABASE_USER || 'root'
+let pass = process.env.DATABASE_PASS || ''
+let dbas = process.env.DATABASE_DBAS || 'waterpark'
+
+module.exports = new (require('sequelize'))(dbas, user, pass, {
+	host    : host,
 	dialect : 'mysql'
 })
